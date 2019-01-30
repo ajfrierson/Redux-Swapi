d4ae5ecf8341efa66dff2787b445b71a21b0a0b1
ajfrierson <alvinfrierson@fuller.edu>
2019-01-30 14:34:33 -0800

    added thunk, logger, and middleware to scr/index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,26 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+
 // needed dependancies
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 // applyMiddleware from redux
+
+
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
+const middleware = applyMiddleware(logger, thunk);
+
+
 const store = createStore(
-  rootReducer
+  rootReducer,
   /* applyMiddleware goes here */
-);
+  middleware
+  );
 
 ReactDOM.render(
   <Provider store={store}>
