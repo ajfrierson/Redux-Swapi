7601d7cfaa3fc87108d57209d465253d1f1ba364
ajfrierson <alvinfrierson@fuller.edu>
2019-01-30 15:01:35 -0800

    added mapStateToProps in CharacterListView.js to debug code

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -29,6 +29,14 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.fetching,
+    error: state.charsReducer.error
+  }
+}
+
 export default connect(
   mapStateToProps /* mapStateToProps replaces null here */,
   {
