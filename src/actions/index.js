e67c543e88f2d35d9f9eac8940c222021eb5c410
ajfrierson <alvinfrierson@fuller.edu>
2019-01-30 14:56:43 -0800

    added action creator to action/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,3 +10,16 @@ export const FAILURE = 'FAILURE';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const getChars = () => dispatch => {
+    dispatch({ type: FETCHING });
+
+    axios
+        .get('https://swapi.co/api/people/')
+        .then(response => {
+            dispatch({ type: SUCCESS, characters: response.data.results})
+        })
+        .catch(err => {
+            dispatch({ type: FAILURE, message: 'Error fetching the data.'})
+        })
+}
